4d95f50ad6b2ee0c1b83ad19ff1ec72c5b675397
KK836 <drwk1640@gmail.com>
2024-07-15 13:47:52 +0900

    [level 0] Title: 분수의 덧셈, Time: 6.77 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/README.md
+++ 프로그래머스/0/120808. 분수의 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 0] 분수의 덧셈 - 120808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.54 ms
+메모리: 36.4 MB, 시간: 6.77 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 12일 18:08:40
+2024년 07월 15일 13:47:43
 
 ### 문제 설명
 
--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,11 +1,20 @@
-function solution(numer1, denom1, numer2, denom2) {
-    
-    // 분자, 분모값을 구한다.
-    let num1 = (numer1 * denom2) + (numer2 * denom1), num2 = denom1 * denom2, max = 1;
-    // 최대 공약수를 구한다.
-    for (let i = 1; i <= num1; i++) {
-        if(num1 % i == 0 && num2 % i == 0)  max = i;
+function solution(number1, denom1, number2 , denom2) {
+  const number = number1 * denom2 + number2 * denom1;
+  const denom = denom1 * denom2;
+  let gcd = 1;
+
+  function getGcd(number, denom) {
+    let smallNumber = number > denom ? denom : number;
+    for (let i = 1; i <= smallNumber; i++) {
+      if (denom % i === 0 && number % i === 0) {
+        gcd = i;
+      }
     }
-    
-    return [num1 / max, num2 / max];
+  }
+  getGcd(number, denom);
+  
+  return [number / gcd, denom / gcd];
 }
+
+
+
